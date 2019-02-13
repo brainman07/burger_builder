de325a21ea55a9a78cf10873e19cc59acd48faa5
brainman07 <weaselwiz7@gmail.com>
2019-02-13 18:13:23 +0200

    Layout HOC

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,3 +1,13 @@
-const layout = () => (
-    
+import React from 'react';
+import Aux from '../../hoc/Aux';
+
+const layout = (props) => (
+    <Aux>
+        <div>Toolbar, SideDrawer, Backdrop</div>
+        <main>
+            {props.children}
+        </main>
+    </Aux>
 );
+
+export default layout;
