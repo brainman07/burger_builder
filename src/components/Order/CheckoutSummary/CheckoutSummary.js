b28b001b317bf5f33ba58107161fee862bfab16f
brainman07 <weaselwiz7@gmail.com>
2019-08-11 17:10:06 +0300

    Add routing for Checkout and BurgerBuilder

--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -13,14 +13,14 @@ const checkoutSummary = (props) => {
 
             <Button
                 btnType="Danger"
-                clicked
+                clicked={props.checkoutCancelled}
             >
                 CANCEL
             </Button>
 
             <Button
                 btnType="Success"
-                clicked
+                clicked={props.checkoutContinued}
             >
                 CONTINUE
             </Button>
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -80,28 +80,39 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        this.setState({ loading: true })
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: 'Andi',
-                address: {
-                    street: 'Here',
-                    zipCode: '41234',
-                    country: 'Romania'
-                },
-                email: 'test@teat.com'
-            },
-            deliveryMethod: 'fastest'
+        const queryParams = []
+        for (let i in this.state.ingredients) {
+            queryParams.push(encodeURIComponent(i) + '=' + 
+            encodeURIComponent(this.state.ingredients[i]))
         }
-        axios.post('/orders.json', order)
-            .then(response => {
-                this.setState({ loading: false, purchasing: false })
-            })
-            .catch(error => {
-                this.setState({ loading: false, purchasing: false })
-            })
+        const queryString = queryParams.join('&')
+
+        this.props.history.push({
+            pathname: '/checkout',
+            search: '?' + queryString
+        })
+        // this.setState({ loading: true })
+        // const order = {
+        //     ingredients: this.state.ingredients,
+        //     price: this.state.totalPrice,
+        //     customer: {
+        //         name: 'Andi',
+        //         address: {
+        //             street: 'Here',
+        //             zipCode: '41234',
+        //             country: 'Romania'
+        //         },
+        //         email: 'test@teat.com'
+        //     },
+        //     deliveryMethod: 'fastest'
+        // }
+        // axios.post('/orders.json', order)
+        //     .then(response => {
+        //         this.setState({ loading: false, purchasing: false })
+        //     })
+        //     .catch(error => {
+        //         this.setState({ loading: false, purchasing: false })
+        //     })
     }
 
     render() {
